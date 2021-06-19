2776e6bd8e2b490cf657870eedab42ce1ca1d801
MengChiehLiu <77425545+MengChiehLiu@users.noreply.github.com>
2021-06-19 23:19:04 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,159 @@
+import os
+import json
+from datetime import datetime
+from flask import Flask, abort, request, render_template
+
+# https://github.com/line/line-bot-sdk-python
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
+#from linebot.models import MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
+user_id=[]
+
+
+@app.route("/", methods=["GET", "POST"])
+def callback():
+
+    if request.method == "GET":
+        return render_template("cover.html")
+    if request.method == "POST":
+        signature = request.headers["X-Line-Signature"]
+        body = request.get_data(as_text=True)
+
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            abort(400)
+
+        return "OK"
+    
+@app.route("/test") #根目錄
+def test():
+    return "This is Test"
+
+@app.route("/forms") #根目錄
+def forms():
+    return render_template("forms.html")
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def talk(event):
+    if event.message.text == "靠北":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="我幹你娘")
+        )
+
+    elif event.message.text == "參加":
+        profile = line_bot_api.get_profile(event.source.user_id)
+        user_id = profile.user_id
+        with open('information.json','r+', newline='') as jsonfile:
+            data = json.load(jsonfile)
+            if user_id in list(data["name_dict"]):
+                line_bot_api.reply_message(event.reply_token,TextSendMessage(text="你已經參加了"))
+            else:
+                line_bot_api.reply_message(event.reply_token,TextSendMessage(text="請輸入你的完整姓名"))
+                a=0
+                while True:
+                    @handler.add(MessageEvent, message=TextMessage)
+                    def detail(event):
+                        user_name = event.message.text
+                        Confirm_template = TemplateSendMessage(
+                            alt_text='目錄 template',
+                            template=ConfirmTemplate(
+                                title='你確定這是你的姓名嗎?',
+                                text='別打錯字拜託',
+                                actions=[                              
+                                    PostbackTemplateAction(
+                                        label='Yes',
+                                        text='Yes',
+                                        data='action=buy&itemid=1'
+                                    ),
+                                    MessageTemplateAction(
+                                        label='N0',
+                                        text='N0'
+                                    )
+                                ]
+                            )
+                        )
+                        line_bot_api.reply_message(event.reply_token,Confirm_template)
+                        @handler.add(MessageEvent, message=TextMessage)
+                        def moredetail(event):
+                            if event.message.text == "Yes":
+                                data.update({user_id:user_name})
+                                line_bot_api.reply_message(event.reply_token,TextSendMessage(text="參加成功"))
+                                break
+                            else:
+                                line_bot_api.reply_message(event.reply_token,TextSendMessage(text="我幹你娘"))
+
+
+
+    elif event.message.text == "id":
+        profile = line_bot_api.get_profile(event.source.user_id)
+        user_pic = profile.picture_url
+        user_id = profile.user_id
+        user_name = profile.display_name
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="你好"+user_name))
+        line_bot_api.push_message(user_id, TextSendMessage(text="你的UD是:"+user_id))
+        line_bot_api.push_message(user_id, TextSendMessage(text="帥喔"))
+        #line_bot_api.push_message(user_id, ImageSendMessage(original_content_url=user_pic, preview_image_url=user_pic))
+    
+    elif event.message.text == "button":
+        buttons_template = TemplateSendMessage(
+            alt_text='Buttons Template',
+            template=ButtonsTemplate(
+                title='這是ButtonsTemplate',
+                text='ButtonsTemplate可以傳送text,uri',
+                thumbnail_image_url='https://ibb.co/qB56zTF',
+                actions=[
+                    MessageTemplateAction(
+                        label='ButtonsTemplate',
+                        text='ButtonsTemplate'
+                    ),
+                    URITemplateAction(
+                        label='開始填寫',
+                        uri='https://nccuacct-angels.herokuapp.com/'
+                    ),
+                    PostbackTemplateAction(
+                        label='postback',
+                        text='postback text',
+                        data='postback1'
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token, buttons_template)
+    elif event.message.text == "YN":
+        print("Confirm template")       
+        Confirm_template = TemplateSendMessage(
+            alt_text='目錄 template',
+            template=ConfirmTemplate(
+                title='這是ConfirmTemplate',
+                text='這就是ConfirmTemplate,用於兩種按鈕選擇',
+                actions=[                              
+                    PostbackTemplateAction(
+                        label='Y',
+                        text='Y',
+                        data='action=buy&itemid=1'
+                    ),
+                    MessageTemplateAction(
+                        label='N',
+                        text='N'
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token,Confirm_template)
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="fail")
+        )
+
