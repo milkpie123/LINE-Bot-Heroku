07b343caa54a330a01a5452477e5c951f599b96e
MengChiehLiu <77425545+MengChiehLiu@users.noreply.github.com>
2021-06-23 03:25:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import json
+import pandas as pd
 from datetime import datetime
 from flask import Flask, abort, request, render_template
 
@@ -57,6 +58,21 @@ def forms():
 def sendresult():
     User_name = request.form.get("User_name")
     content = request.form.get("content")
+    
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    sql = "select * from account;"
+    dat = pd.read_sql_query(sql, conn)
+    conn = None
+    try:
+        fliter = (dat["username"]==User_name)
+        UID = dat[fliter]["user_id"][0]
+        line_bot_api.push_message(UID, TextSendMessage(text=content))
+        return render_template("success.html")
+    except:
+        return render_template("fail.html")
+    
+    
+'''
     def get_key(val):             
         for key, value in data["name_dict"].items(): 
             if val == value:
@@ -69,6 +85,7 @@ def sendresult():
         return render_template("success.html")
     except:
         return render_template("fail.html")
+'''
 
 
     
