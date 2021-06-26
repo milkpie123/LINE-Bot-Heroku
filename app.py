364d91d72cc2707ba8fbe1d81118a8d7b0a24a59
Meng-Chieh, Liu <77425545+MengChiehLiu@users.noreply.github.com>
2021-06-26 16:58:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ def talk(event):
         try:
             conn = psycopg2.connect(DATABASE_URL, sslmode='require')
             cursor = conn.cursor()
-            cursor.execute(f"""DELETE FROM account WHERE id = {user_id};""")
+            cursor.execute(f"""DELETE FROM account WHERE user_id = {user_id};""")
             conn.commit()
             cursor.close()
             conn.close()
